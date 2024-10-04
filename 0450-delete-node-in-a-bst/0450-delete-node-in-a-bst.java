93c80a44faaa2b9fc5265b9c9b62399a802623d4
Prabhat Suman <32027224+prabhatsuman@users.noreply.github.com>
2024-10-04 11:39:07 +0530

    Time: 0 ms (100%), Space: 45.7 MB (88.48%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root)
+    {
+        while(root.left!=null)
+            root=root.left;
+        return root;
+
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null)return root;
+
+        if(root.val>key)root.left=deleteNode(root.left,key);
+        else if (root.val<key) root.right=deleteNode(root.right,key);
+        else 
+        {
+            //case 1-> when root is present only;
+            if(root.left==null && root.right==null)return null;
+            else if(root.left==null) return root.right;
+            else if(root.right==null) return root.left;
+            else 
+            {
+                TreeNode temp=inorderSuccessor(root.right);
+                root.val=temp.val;
+                root.right=deleteNode(root.right,temp.val);
+            }
+        }
+        return root;
+    }
+}
